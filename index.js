1539b27a938241710b25ab454a6e2b83b6f4cb37
Mike DuRussel <mdurussel@squarespace.com>
2020-12-03 21:46:33 -0500

    Use discord

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const Discord = require('discord.js');
 const app = express()
 const port = process.env.PORT || 3000;
+
+const discordServer = new Discord.WebhookClient(
+  process.env.DISCORD_WEBHOOK_ID,
+  process.env.DISCORD_WEBHOOK_TOKEN,
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,9 +19,13 @@ app.post('/', (req, res) => {
   console.log(req.body);
   console.log(req.body.app);
   console.log(req.body.release);
-  res.send('Hello World!')
-})
+  const app = req.body.app;
+  const user = req.body.user;
+  const release = req.body.release;
+  hook.send(`**${app}** has been promoted to **${release}** by **${user}**.`);
+  res.send('Success.');
 
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
