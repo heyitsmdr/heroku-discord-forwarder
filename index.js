38d390339020e55935349b967c8fb1a041252c93
Mike DuRussel <mdurussel@squarespace.com>
2020-12-03 21:33:53 -0500

    Try accessing fields

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   console.log(req.body);
+  console.log(req.body.app);
+  console.log(req.body.release);
   res.send('Hello World!')
 })
 
