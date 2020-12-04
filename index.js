a6144fcd39e4a1a38d8e306f1ccc69a090e63c27
Mike DuRussel <mdurussel@squarespace.com>
2020-12-03 21:32:28 -0500

    Use body-parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
